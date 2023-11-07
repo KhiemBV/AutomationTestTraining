a3a436b66bd67af59562141a830da80f05ecd95c
KhiemBV <khiema494@gmail.com>
2023-11-07 17:26:21 +0700

    update day 7

--- src/main/java/automation/common/CommonBase.java
+++ src/main/java/automation/common/CommonBase.java
@@ -1,5 +1,21 @@
 package automation.common;
 
-public class CommonBase {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CommonBase {
+	public WebDriver driver;
+
+	public WebDriver initChromeDriver(String URL) {
+		ChromeOptions options = new ChromeOptions();
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+		driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get(URL);
+		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+		return driver;
+	}
 }
--- src/main/java/automation/testsuite/Login.java
+++ src/main/java/automation/testsuite/Login.java
@@ -1,5 +1,19 @@
 package automation.testsuite;
 
-public class Login {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
 
+import automation.common.CommonBase;
+
+public class Login extends CommonBase{
+
+	@BeforeTest
+	public void openChrome() {
+		driver = initChromeDriver("https://bepantoan.vn/");
+	}
+	
+	@Test
+	public void test() {
+		System.out.println("Hello my first project!");
+	}
 }
